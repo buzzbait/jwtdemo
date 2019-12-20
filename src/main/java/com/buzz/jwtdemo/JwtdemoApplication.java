f3f4372cd1bfa2b299e3baa91bfbb48c652290db
buzz <buzzbait@naver.com>
2019-12-20 17:57:22 +0900

    수정

--- src/main/java/com/buzz/jwtdemo/JwtdemoApplication.java
+++ src/main/java/com/buzz/jwtdemo/JwtdemoApplication.java
@@ -3,8 +3,10 @@ package com.buzz.jwtdemo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 @SpringBootApplication
+@EnableTransactionManagement
 @EnableGlobalMethodSecurity(prePostEnabled = true)
 public class JwtdemoApplication {
 
